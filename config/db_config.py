d1dd515d9d682717ecb4270678186473a284208a
nicho1607 <nicolacho1607@gmail.com>
2025-04-29 21:13:20 -0500

    sub1 terminado

--- config/db_config.py
+++ config/db_config.py
@@ -1,8 +1,8 @@
 class DBConfig:
     def __init__(self):
-        self.server = 'nico\\SQLEXPRESS'  # Ajusta según tu instancia
-        self.database = 'PreInscripcionesUniversidad'           # Nombre de tu BD en SQL Server
-        self.username = 'sa'          # Ej: 'sa'
+        self.server = 'nico\\SQLEXPRESS'  
+        self.database = 'PreInscripcionesUniversidad'           
+        self.username = 'sa'          
         self.password = 'nico'
         self.driver = '{ODBC Driver 18 for SQL Server}'  # Versión 18
         
@@ -13,5 +13,5 @@ class DBConfig:
             f"DATABASE={self.database};"
             f"UID={self.username};"
             f"PWD={self.password};"
-            f"Encrypt=no;"  # Opcional: 'yes' para conexiones seguras
+            f"Encrypt=no;"  
         )
